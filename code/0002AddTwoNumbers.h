1fbbc622796b2309b23f9001df0c40192efc710c
Chaobin Yang <chbyangchbyang@gmail.com>
2018-12-14 20:17:46 -0500

    Update 0002AddTwoNumbers.h

--- code/0002AddTwoNumbers.h
+++ code/0002AddTwoNumbers.h
@@ -1,5 +1,7 @@
 /*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -25,6 +27,8 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
     {
+     //每次carry记录是否进位， 如果l1 l2 carry不为NULL，向下一位生成，
+     //时刻注意，l1，l2是否已经是NULL
         ListNode dummyHead(0);
         ListNode *current=&dummyHead;
         int carry=0;
