066d332e29d632780d8554fda399ba1838953218
Chaobin Yang <chbyangchbyang@gmail.com>
2018-12-14 21:14:35 -0500

    Update SlidingWindow1.cpp

--- template/SlidingWindow1.cpp
+++ template/SlidingWindow1.cpp
@@ -3,13 +3,16 @@
         int start=0;
         int res=-1;
         for(int i=0;i<tree.size();i++){
+            //更新start位置
             if(m.size()==k && m.count(tree[i])==0)
                 while(m.size()==k){
                     if(--m[tree[start]]==0)
                         m.erase(tree[start]);
                     start++;
                 }
+            //更新hash map
             m[tree[i]]++;
+            //更新result
             res=max(res,i-start+1);
         }
         return res;
