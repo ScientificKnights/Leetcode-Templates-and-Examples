b40276447e277ff330b8e544d5f2e265bef5ad39
Chaobin Yang <chbyangchbyang@gmail.com>
2019-02-24 17:55:21 -0500

    Create 0338Counting Bits.h

---
+++ code/0338Counting Bits.h
@@ -0,0 +1,15 @@
+/*
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's 
+in their binary representation and return them as an array.
+Example 1:
+Input: 5
+Output: [0,1,1,2,1,2]
+*/
+
+vector<int> countBits(int num) {
+//result[i]=result[i/2]+i%2
+    vector<int> result(num+1,0);
+    for(int i=1;i<num+1;i++)
+        result[i]=result[i/2]+i%2;
+    return result;
+}
