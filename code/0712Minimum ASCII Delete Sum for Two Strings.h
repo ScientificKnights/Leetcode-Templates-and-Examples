829c117b11805cfac6753f102784e3260e75f52f
Chaobin Yang <chbyangchbyang@gmail.com>
2019-02-25 15:47:04 -0500

    Update 0712Minimum ASCII Delete Sum for Two Strings.h

--- code/0712Minimum ASCII Delete Sum for Two Strings.h
+++ code/0712Minimum ASCII Delete Sum for Two Strings.h
@@ -16,25 +16,29 @@ int minimumDeleteSum(string s1, string s2) {
     e   dle dl      dle         dlet
     l   de  dll/dee deee/dlle   deeet/dllet
     */
-    //相同72. Edit Distance
     //相同dp[i][j]=dp[i-1][j-1]
     //不同 从另两个位置过来
     int n=s1.size(),m=s2.size();
     vector<vector<int>> dp(n,vector<int>(m,0));
+    
+    //初始化 0,0
     bool equal=true;
     if(s1[0]!=s2[0]) {dp[0][0]=s1[0]+s2[0]; equal=false;}
 
+    //初始化第一行
     for(int i=1;i<m;i++){
         if(s1[0]==s2[i] && equal==false) {dp[0][i]=dp[0][i-1]-s1[0]; equal=true;}
         else dp[0][i]=dp[0][i-1]+s2[i];
     }
 
+    //初始化 0，0 初始化第一列
     equal=(s1[0]==s2[0]);
     for(int i=1;i<n;i++){
         if(s2[0]==s1[i] && equal==false) {dp[i][0]=dp[i-1][0]-s2[0]; equal=true;}
         else dp[i][0]=dp[i-1][0]+s1[i];
     }
 
+    //相同 或者 不同(删某一个)
     for(int i=1;i<n;i++)
         for(int j=1;j<m;j++){
             if(s1[i]==s2[j])
