19c82411f6e5d76a0c928e70cdbec3f365420555
Chaobin Yang <chbyangchbyang@gmail.com>
2019-03-08 16:36:04 -0500

    Update BinarySearch2.cpp

--- template/BinarySearch2.cpp
+++ template/BinarySearch2.cpp
@@ -1,11 +1,14 @@
 //Implement int sqrt(int x).
 
 int left=1, right=x,mid;
-while(left<right){
+while(left<right){  
     mid=left+(right-left)/2;
-    if(mid>x/mid)		//先判断右侧是否满足，update r, 少用乘法，防止越界；
+    //先判断右侧是否满足，update r, 少用乘法，防止越界；
+    //如果mid>(). 找到的left>target; left-1<=target; return left-1
+    //如果mid>=(). 找到的left>=target; left-1<target; return left
+    if(mid>x/mid)
         right=mid;
     else
-        left=mid+1;		
+        left=mid+1;	
 }
-return start-1;		//因为一定能找到，所以while loop外返回，start-1是没发生相变最大的，start是发生相变后第一个
+return left-1;		//因为一定能找到，所以while loop外返回，left-1是没发生相变最大的，left是相变发生后的
