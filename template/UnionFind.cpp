6c67963c64d798b6f60d4f49fd9cb0f6b8c69539
Chaobin Yang <chbyangchbyang@gmail.com>
2019-03-08 19:46:08 -0500

    Create UnionFind.cpp

---
+++ template/UnionFind.cpp
@@ -0,0 +1,24 @@
+class Unionfind{
+private:
+    vector<int> root;
+public:
+    //constructor
+    Unionfind(int n){
+        root=vector<int>(n+1,0);
+        for(int i=0;i<n+1;i++)
+            root[i]=i;
+    }
+    //Union
+    bool Union(int u,int v){
+        int root_u=findroot(u),root_v=findroot(v);
+        if(root_u==root_v) return false;
+        root[root_u]=root_v;
+        return true;
+    }
+    //find root
+    int findroot(int u){
+        if(u!=root[u])
+            root[u]=findroot(root[u]);      //path compression
+        return root[u];
+    }
+};
