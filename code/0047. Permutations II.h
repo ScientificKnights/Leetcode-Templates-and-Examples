8c2cc84b46f2b8b31462afbfb3c07214770800c8
Chaobin Yang <chbyangchbyang@gmail.com>
2019-03-11 13:57:11 -0400

    Update 0047. Permutations II.h

--- code/0047. Permutations II.h
+++ code/0047. Permutations II.h
@@ -3,9 +3,7 @@ Given a collection of numbers that might contain duplicates, return all possible
 
 Input: [1,1,2]
 Output:
-[  [1,1,2],
-  [1,2,1],
-  [2,1,1] ]
+[  [1,1,2],  [1,2,1],  [2,1,1] ]
 */
 void helper(vector<vector<int>> &res, vector<int>& nums, int length, int cur){
     if(cur==length-1){
