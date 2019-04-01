f319ce328e8d8cf2735834d35a03903622185226
Chaobin Yang <chbyangchbyang@gmail.com>
2019-04-01 13:13:18 -0400

    Update 0576. Out of Boundary Paths.h

--- code/0576. Out of Boundary Paths.h
+++ code/0576. Out of Boundary Paths.h
@@ -1,3 +1,4 @@
+/*
 There is an m by n grid with a ball. Given the start coordinate (i,j) of the ball, 
 you can move the ball to adjacent cell or cross the grid boundary in four directions (up, down, left, right). 
 However, you can at most move N times. Find out the number of paths to move the ball out of grid boundary. 
