8798656a6e2276f693a874229aa8834f375d8592
Chaobin Yang <chbyangchbyang@gmail.com>
2019-04-11 13:55:26 -0400

    Create 0783. Minimum Distance Between BST Nodes.h

---
+++ code/0783. Minimum Distance Between BST Nodes.h
@@ -0,0 +1,40 @@
+/*
+Given a Binary Search Tree (BST) with the root node root, 
+return the minimum difference between the values of any two different nodes in the tree.
+
+Input: root = [4,2,6,1,3,null,null]
+Output: 1
+The given tree [4,2,6,1,3,null,null] is represented by the following diagram:
+
+          4
+        /   \
+      2      6
+     / \    
+    1   3  
+
+while the minimum difference in this tree is 1, it occurs between node 1 and node 2, also between node 3 and node 2.
+The size of the BST will be between 2 and 100.
+The BST is always valid, each node's value is an integer, and each node's value is different.
+*/
+
+class Solution {
+private:
+    bool flag=0;    //whether first run or not
+    long last;
+    long res=INT_MAX;
+    void diff(TreeNode* root){
+        if(!root) return;
+        diff(root->left);
+        if(flag)        //not first time
+            res=min(res,(long)root->val-last);
+        last=root->val;
+        flag=1;
+        diff(root->right);
+        return;
+    }
+public:
+    int minDiffInBST(TreeNode* root) {
+        diff(root);
+        return (int)res;
+    }
+};
