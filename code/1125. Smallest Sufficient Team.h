380bc68684b4b143f5865808022395c043aed015
Chaobin Yang <chbyangchbyang@gmail.com>
2019-07-14 14:39:42 -0700

    Update 1125. Smallest Sufficient Team.h

--- code/1125. Smallest Sufficient Team.h
+++ code/1125. Smallest Sufficient Team.h
@@ -3,15 +3,14 @@ In a project, you have a list of required skills req_skills, and a list of peopl
 The i-th person people[i] contains a list of skills that person has.
 
 Consider a sufficient team: a set of people such that for every required skill in req_skills, 
-there is at least one person in the team who has that skill.  We can represent these teams by the index of each person: for example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
+there is at least one person in the team who has that skill.  We can represent these teams by the index of each person: 
+for example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
 
 Return any sufficient team of the smallest possible size, represented by the index of each person.
-
 You may return the answer in any order.  It is guaranteed an answer exists.
 
 Input: req_skills = ["java","nodejs","reactjs"], people = [["java"],["nodejs"],["nodejs","reactjs"]]
 Output: [0,2]
-
 Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
 Output: [1,2]
  
