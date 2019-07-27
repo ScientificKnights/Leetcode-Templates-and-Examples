fd0dce09d547a4b2a64c884d60cf3207de32494f
Chaobin Yang <chbyangchbyang@gmail.com>
2019-07-27 14:52:45 -0700

    Create 0011. Container With Most Water.h

---
+++ code/0011. Container With Most Water.h
@@ -0,0 +1,29 @@
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, 
+which together with x-axis forms a container, such that the container contains the most water.
+Note: You may not slant the container and n is at least 2.
+
+The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, 
+the max area of water (blue section) the container can contain is 49.
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+*/
+
+int maxArea(vector<int>& height) {
+    //greedy
+    //move short line can higher in height although lower in width
+    int n=height.size();
+    if(n<=1) return 0;
+    int i=0,j=n-1;
+    int res=0;
+    while(i<j){
+        res=max(res,(j-i)*min(height[i],height[j]));
+        if(height[i]>height[j])
+            j--;
+        else
+            i++;
+    }
+    return res;
+}
