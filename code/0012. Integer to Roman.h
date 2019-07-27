452d256512e678d872eb0310fad1155562257990
Chaobin Yang <chbyangchbyang@gmail.com>
2019-07-27 16:27:46 -0700

    Create 0012. Integer to Roman.h

---
+++ code/0012. Integer to Roman.h
@@ -0,0 +1,53 @@
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. 
+Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+
+Input: 3
+Output: "III"
+
+Input: 4
+Output: "IV"
+
+Input: 9
+Output: "IX"
+
+Input: 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+
+Input: 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+string intToRoman(int num) 
+{   
+    const string THOUS[]= {"","M","MM","MMM"};
+    const string HUNDS[]= {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+    const string TENS[]	= {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+    const string ONES[]	= {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+    string result;
+    result += THOUS[(int)(num/1000)%10];
+    result += HUNDS[(int)(num/100)%10];
+    result += TENS[(int)(num/10)%10];
+    result += ONES[num%10];
+    return result;
+}
