e69279f809107786640a209e89263b4bf71e759a
ScientificKnights <53413237+ScientificKnights@users.noreply.github.com>
2019-09-09 23:23:20 -0400

    Create 362. Design Hit Counter.h

---
+++ code/362. Design Hit Counter.h
@@ -0,0 +1,62 @@
+/*
+Design a hit counter which counts the number of hits received in the past 5 minutes.
+Each function accepts a timestamp parameter (in seconds granularity)
+and you may assume that calls are being made to the system in chronological order
+(ie, the timestamp is monotonically increasing). You may assume that the earliest timestamp starts at 1.
+
+It is possible that several hits arrive roughly at the same time.
+
+HitCounter counter = new HitCounter();
+// hit at timestamp 1.
+counter.hit(1);
+// hit at timestamp 2.
+counter.hit(2);
+// hit at timestamp 3.
+counter.hit(3);
+// get hits at timestamp 4, should return 3.
+counter.getHits(4);
+// hit at timestamp 300.
+counter.hit(300);
+// get hits at timestamp 300, should return 4.
+counter.getHits(300);
+// get hits at timestamp 301, should return 3.
+counter.getHits(301); 
+Follow up:
+What if the number of hits per second could be very large? Does your design scale?
+*/
+
+class HitCounter {
+private:
+    queue<pair<int,int>> q;
+    int count;
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        count=0;
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        count++;
+        if(!q.empty()&&q.back().first==timestamp) q.back().second++;
+        else q.push({timestamp,1});
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        while(!q.empty() && q.front().first<=timestamp-300){
+            count-=q.front().second;
+            q.pop();
+        }
+        return count;
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
