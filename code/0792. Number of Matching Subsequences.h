ddb5074f2cb969146239bf40af7576dad901a52d
ScientificKnights <53413237+ScientificKnights@users.noreply.github.com>
2019-09-17 12:05:55 -0400

    Update 0792. Number of Matching Subsequences.h

--- code/0792. Number of Matching Subsequences.h
+++ code/0792. Number of Matching Subsequences.h
@@ -13,11 +13,11 @@ int numMatchingSubseq(string S, vector<string>& words) {
     for(int i=0;i<n;i++)
         data[S[i]-'a'].push_back(i);
     for(int i=0;i<words.size();i++){
-        int prev=0;
+        int prev=-1;
         for(int j=0;j<words[i].size();j++){
-            auto cur=lower_bound(data[words[i][j]-'a'].begin(),data[words[i][j]-'a'].end(),prev);
+            auto cur=upper_bound(data[words[i][j]-'a'].begin(),data[words[i][j]-'a'].end(),prev);
             if(cur!=data[words[i][j]-'a'].end() )
-                prev=(*cur)+1;
+                prev=(*cur);
             else break;
 
             if(j==words[i].size()-1) res++;
